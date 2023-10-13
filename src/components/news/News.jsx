c045874d8f38b8c9aca9fbfb31fa55f23a3a6f48
Nicholai <nichobech02@gmail.com>
2023-10-13 12:28:09 +0200

    CHANGES

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,21 +1,33 @@
-import image1 from "../../img/techwizard.avif"
+import image1 from "../../img/techwizard.avif";
+import image2 from "../../img/mudwizard.jpg";
 
-const news = () =>{
-    return(
-        <div className="container news-container">
-        <section className="row ">
-          <div className="col-md-6 ">
-            <h2 className="text-center">Section 1</h2>
-            <figure><img className="img-fluid justify-content-center" src={image1} alt="" /></figure>
-          </div>
-          <div className="col-md-6">
-            <h2 className="text-center">Section 2</h2>
-            <figure><img className="img-fluid justify-content-center" src={image1} alt="" /></figure>
-          </div>
-        </section>
-      </div>
-    );
-}
-
+const news = () => {
+  return (
+    <div className="container news-container">
+      <section className="row ">
+        <div className="col-md-6 ">
+          <figure>
+            <img
+              className="img-fluid justify-content-center section_container"
+              src={image1}
+              alt=""
+            />
+          </figure>
+          <h2 className="text-center">New in the world of wizarding</h2>
+        </div>
+        <div className="col-md-6">
+          <figure>
+            <img
+              className="img-fluid justify-content-center section_container"
+              src={image2}
+              alt=""
+            />
+          </figure>
+          <h2 className="text-center justify-content-center">Scandalous wizard buisness</h2>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default news;
